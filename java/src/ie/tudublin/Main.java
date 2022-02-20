ac2b800499a089e485fdfde937e90d253a383ce7
L Fernandez <72343133+vedez@users.noreply.github.com>
2022-02-20 15:45:34 +0000

    HelloProccessing

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -8,18 +8,6 @@ public class Main
         processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
 
-    public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-    public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
     public void cat()
     {
         System.out.println("Hello world");
@@ -45,10 +33,6 @@ public class Main
     }
     public static void main(String[] args)
     {
-        loops();
-
-        // Tara Misty
-        // Tara Tara
-        
+        helloProcessing();
     }
 }
