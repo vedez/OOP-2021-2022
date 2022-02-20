c7a55d2c3ddc3cd69f30aeea6cf3733421a768f9
L Fernandez <72343133+vedez@users.noreply.github.com>
2022-02-20 20:00:20 +0000

    lab2 pause

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,37 +2,14 @@ package ie.tudublin;
 
 public class Main
 {
-    public static void helloProcessing()
+    public static void bugZap()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
+        processing.core.PApplet.runSketch( a, new BugZap());
     }
 
-    public void cat()
-    {
-        System.out.println("Hello world");
-
-        Animal misty = new Animal("Misty");
-        Animal lucy = new Animal("Lucy");
-
-        lucy = misty;
-        misty.setName("Tara");
-
-        System.out.println(misty);
-        System.out.println(lucy);
-
-        
-        Cat cat = new Cat("Ginger");
-
-        while(cat.getNumLives() > 0)
-        {
-            cat.kill();
-        }
-        cat.kill();
-
-    }
     public static void main(String[] args)
     {
-        helloProcessing();
+        bugZap();
     }
 }
