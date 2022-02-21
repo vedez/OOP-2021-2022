0f120f031598c24029880ee96ec07060ec72fcbf
L Fernandez <72343133+vedez@users.noreply.github.com>
2022-02-21 02:25:20 +0000

    Refined version provided by BDuggan

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -2,104 +2,137 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class BugZap extends PApplet 
-{
-	float playerX, playerY, playerWidth;
-
-	float bugX, bugY, bugWidth;
-
-	//Setting screen size
+public class BugZap extends PApplet {
 	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
-		colorMode(RGB);
-		smooth();
-
-		//Player setup
-		playerX = width/2;
-		playerY = height - 50;
-		playerWidth = 50;
-
-		resetBug();
+		reset();
 	}
 
-	private void resetBug() 
-	{
-		bugX = random(bugWidth/2, width - (bugWidth/2));
-		bugY = 50;
-		bugWidth = 50;
-	}
-
-	void drawBug(float x, float y, float w) 
-	{
-		float halfW = w/2;
-		stroke(255);
-		noFill();
-		triangle(x - halfW, y + halfW, x, y -  halfW, x + halfW, y + halfW);
-	}
-
-	void drawPlayer(float x, float y, float w) 
-	{
-		stroke(255);
-		noFill();
-		rectMode(CENTER);
-		rect(x, y, w, 20);
-		line(x,y - 10, x, y - 20);
-	}
-
-	//ref: keyPressed()
+	float playerX, playerY;
 	float playerSpeed = 5;
+	float playerWidth = 40;
+	float halfPlayerWidth = playerWidth / 2;
 
-	public void keyPressed()
-	{
-		if (keyCode == LEFT) 
-		{
-			//playerX--;
-			playerX -= playerSpeed;
-		}
-
-		if (keyCode == RIGHT) 
-		{
-			//playerX++;
-			playerX += playerSpeed;
-		}
-
-		if (key == ' ')
-		{
-			float halfW = bugWidth/2;
-			if (playerX > bugX - halfW && playerX < bugX + halfW)
-			{
-				score++;
-				resetBug();
-				line(playerX, playerY - 10, playerX, bugY);
-			}
-			else{
-				line(playerX, playerY - 10, playerX, 0);
-			}
-		}
-	}
+	float bugX, bugY, bugWidth = 30;
+	float halfBugWidth = bugWidth / 2;
 
 	int score = 0;
 
-	void moveBug()
-	{
-		bugY++;
-		bugX += random(-20,20);
+	void reset() {
+		resetBug();
+		playerX = width / 2;
+		playerY = height - 50;
 	}
-	
-	public void draw()
-	{
-		background(0);
-		strokeWeight(2);
-		drawPlayer(playerX, playerY, playerWidth);
-		drawBug(bugX, bugY, bugWidth);
 
-		if (frameCount % 20 == 0) 
-		{
-			moveBug();
+	void resetBug() {
+		bugX = random(halfBugWidth, width - halfBugWidth);
+		bugY = 50;
+	}
+
+	void drawBug(float x, float y) {
+		// Draw the bug
+		stroke(255);
+		float saucerHeight = bugWidth * 0.7f;
+		line(x, y - saucerHeight, x - halfBugWidth, y);
+		line(x, y - saucerHeight, x + halfBugWidth, y);
+		// line(x - halfBugWidth, y, x - halfBugWidth, y);
+		line(x - halfBugWidth, y, x + halfBugWidth, y);
+		float feet = bugWidth * 0.1f;
+		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
+		line(x + feet, y, x + halfBugWidth, y + halfBugWidth);
+
+		feet = bugWidth * 0.3f;
+		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
+		line(x + feet, y, x + halfBugWidth, y + halfBugWidth);
+
+		float eyes = bugWidth * 0.1f;
+		line(x - eyes, y - eyes, x - eyes, y - eyes * 2f);
+		line(x + eyes, y - eyes, x + eyes, y - eyes * 2f);
+
+	}
+
+	void drawPlayer(float x, float y, float w) {
+		stroke(255);
+		float playerHeight = w / 2;
+		line(x - halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight);
+		line(x - halfPlayerWidth, y + playerHeight, x - halfPlayerWidth, y + playerHeight * 0.5f);
+		line(x + halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight * 0.5f);
+
+		line(x - halfPlayerWidth, y + playerHeight * 0.5f, x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+		line(x + halfPlayerWidth, y + playerHeight * 0.5f, x + (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+
+		line(x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f, x + (halfPlayerWidth * 0.8f),
+				y + playerHeight * 0.3f);
+
+		line(x, y, x, y + playerHeight * 0.3f);
+
+	}
+
+	public void keyPressed() {
+		if (keyCode == LEFT) {
+			if (playerX > halfPlayerWidth) {
+				playerX -= playerSpeed;
+			}
 		}
-		text("Score: " + score, 50, 50);
+		if (keyCode == RIGHT) {
+			if (playerX < width - halfPlayerWidth) {
+				playerX += playerSpeed;
+			}
+		}
+		if (keyCode == ' ')
+		{
+			if (playerX > bugX - halfBugWidth && playerX < bugX + halfBugWidth)
+			{
+				line(playerX, playerY, playerX, bugY);
+				score ++;
+				resetBug();
+			}
+			else
+			{
+				line(playerX, playerY, playerX, 0);
+			}
+		}
+	}
+
+	void moveBug() {
+		if ((frameCount % 30) == 0) {
+			bugX += random(-5, 5);
+			if (bugX < halfBugWidth) {
+				bugX = halfBugWidth;
+			}
+			if (bugX > width - halfBugWidth) {
+				bugX = width - halfBugWidth;
+			}
+			bugY += 2;
+		}
+	}
+
+	int gameMode = 0;
+
+	public void draw() {
+		background(0);
+		if (gameMode == 0)
+		{
+			fill(255);
+			drawPlayer(playerX, playerY, playerWidth);
+			drawBug(bugX, bugY);
+			moveBug();
+
+			text("Score: " + score, 20, 20);
+		}
+		else
+		{
+			textAlign(CENTER, CENTER);
+			text("GAME OVER!!!", width / 2, height / 2);
+		}
+
+		if (bugY > height - 50)
+		{
+			gameMode = 1;
+		}
+
 	}
 }
