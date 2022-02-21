dba1a0564b62d5ab64c309df99507c55ed215377
L Fernandez <72343133+vedez@users.noreply.github.com>
2022-02-21 08:16:51 +0000

    Loops Lab

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,53 +2,14 @@ package ie.tudublin;
 
 public class Main
 {
-    public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-    public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
     public static void loops()
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Loops());
     }
-
-    public void cat()
-    {
-        System.out.println("Hello world");
-
-        Animal misty = new Animal("Misty");
-        Animal lucy = new Animal("Lucy");
-
-        lucy = misty;
-        misty.setName("Tara");
-
-        System.out.println(misty);
-        System.out.println(lucy);
-
-        
-        Cat cat = new Cat("Ginger");
-
-        while(cat.getNumLives() > 0)
-        {
-            cat.kill();
-        }
-        cat.kill();
-
-    }
     public static void main(String[] args)
     {
         loops();
 
-        // Tara Misty
-        // Tara Tara
-        
     }
 }
